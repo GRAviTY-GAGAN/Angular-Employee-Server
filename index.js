1ad98c1b6208b6339df48e59328031f661e22fcc
GRAviTY-GAGAN <gaganbn1999@gmal.com>
2023-08-09 09:45:59 +0530

    update

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const { connection } = require("./db");
+const cors = require("cors");
 const { employeeRouter } = require("./Routes/employee.routes");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT;
 
+app.use(cors());
 app.use(express.json());
 app.use("/employee", employeeRouter);
 
